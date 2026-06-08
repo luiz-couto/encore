844ddbf7e669307eaa3d542cdd85f0ff369f2e86
luizz-couto <luiz@yalo.com>
2026-06-08 10:12:12 +0100

    improvements

--- encore/scripts/conductor.gd
+++ encore/scripts/conductor.gd
@@ -1,6 +1,6 @@
 extends Node
 
-@export var bpm: float = 115:
+@export var bpm: float = 45:
 	set(value):
 		bpm = value
 		secondsPerBeat = 60.0 / bpm
--- encore/scripts/music_player.gd
+++ encore/scripts/music_player.gd
@@ -2,21 +2,27 @@ extends Node
 
 # Semitone offsets from C4 for each chord (root, third, fifth)
 const CHORD_NOTES: Dictionary = {
-	0: [0,  4,  7,  12], # C maj: C4, E4, G4, C5
-	1: [2,  5,  9,  14], # D min: D4, F4, A4, D5
-	2: [4,  7,  11, 16], # E min: E4, G4, B4, E5
-	3: [5,  9,  12, 17], # F maj: F4, A4, C5, F5
-	4: [7,  11, 14, 19], # G maj: G4, B4, D5, G5
-	5: [9,  12, 16, 21], # A min: A4, C5, E5, A5
-	6: [11, 14, 17, 23], # B dim: B4, D5, F5, B5
+	0: [0,   4,   7,  12], # C maj: C4, E4, G4, C5
+	1: [-10, -7,  -3,  2], # D min: D3, F3, A3, D4
+	2: [-8,  -5,  -1,  4], # E min: E3, G3, B3, E4
+	3: [-7,  -3,   0,  5], # F maj: F3, A3, C4, F4
+	4: [-5,  -1,   2,  7], # G maj: G3, B3, D4, G4
+	5: [-3,   0,   4,  9], # A min: A3, C4, E4, A4
+	6: [-1,   2,   5, 11], # B dim: B3, D4, F4, B4
 }
 
 func play_chord(chord_idx: int) -> void:
 	var notes = CHORD_NOTES[chord_idx];
 	for i in 4:
 		var player: AudioStreamPlayer2D = get_child(i);
-		player.pitch_scale = pow(2.0, notes[i] / 12.0);
-		player.play();
+		var newScale = pow(2.0, notes[i] / 12.0);
+		if not player.playing or player.pitch_scale != newScale:
+			player.pitch_scale = newScale;
+			player.volume_db = -20.0
+			player.play();
+			var tween = create_tween();
+			tween.tween_property(player, "volume_db", 0.0, 0.4)
+
 
 func play_note(chord_idx: int, lane: int) -> void:
 	var semitones = CHORD_NOTES[chord_idx][lane];
