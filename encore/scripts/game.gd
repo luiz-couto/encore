c31b0b2a28da808d085a2444f888eed09c182baa
luizz-couto <luiz@yalo.com>
2026-06-14 13:37:21 +0100

    structural engine

--- encore/scripts/game.gd
+++ encore/scripts/game.gd
@@ -2,12 +2,15 @@ extends Node2D
 
 const NoteScene := preload("res://scenes/note.tscn");
 
-var currentChord: int = 0;
+var currentChord: int = 0
+var currentSection = 0
+var currentIntensity: float = 0.2
 
 func _on_conductor_measure(measurePosition: int) -> void:
 	if measurePosition == 1:
 		currentChord = $ChordGenerator.advance();
 		$ChordGenerator.maybeRefresh();
+		$StructuralEngine.advance_bar();
 
 	$MusicPlayer.play_chord(currentChord);
 	spawnNote(currentChord);
@@ -24,3 +27,9 @@ func _on_score_event(scorePoints: int) -> void:
 
 func _on_timer_timeout() -> void:
 	pass # Replace with function body.
+
+func _on_structural_engine_section_changed(section: Variant, intensity: Variant) -> void:
+	currentSection = section
+	currentIntensity = intensity
+	$ChordGenerator.pick_progression()
+	print(currentSection, " ", currentIntensity);
